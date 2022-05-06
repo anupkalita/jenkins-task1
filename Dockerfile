3ab8bc728fad5046051fd9499ded1c89bc463f0a
anup <ajyotikalita@presidio.com>
2022-05-06 14:11:02 +0530

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:13-alpine
 RUN mkdir -p /home/app
 COPY ./* /home/app/
-EXPOSE 8081
 WORKDIR /home/app
 RUN npm install
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-var http = require("http");
+// var http = require("http");
 
-http.createServer(function (request, response) {
+// http.createServer(function (request, response) {
 
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   response.end('Hello World\n');
-}).listen(8081);
+//    response.end('Hello World\n');
+// }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+// console.log('Server running at http://127.0.0.1:8081/');
+
+const express = require("express");
+
+const app = express();
+
+//define port
+const port=3000;
+
+app.get("/", (req, res) => {
+
+res.json({message:'Root page'})
+
+})
+
+//get example
+
+app.get("/get-data", (req, res) => {
+
+res.json({message:'Get JSON Example'})
+
+})
+
+//run the application
+app.listen(port, () => {
+  console.log(`running at port ${port}`);
+});
